5daa7b4a75ab40fbc1bbc511612b849365099233
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-06 16:54:41 +0300

    add JWTAuthentication view

--- user/api/views.py
+++ user/api/views.py
@@ -24,6 +24,29 @@ class Register(APIView):
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
 
+class RegisterWithJWT(APIView):
+    def post(self, request):
+        serializer = RegisterationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()  # save method in serializer returns the user object
+            data['response'] = 'Successfully registered a new user.'
+            data['username'] = account.username
+            data['email'] = account.email
+
+            from rest_framework_simplejwt.tokens import RefreshToken
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
+
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            data = serializer.errors
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+
 class Logout(APIView):
     def post(self, request):
         request.user.auth_token.delete()
