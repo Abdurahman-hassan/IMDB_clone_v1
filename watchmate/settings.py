b810416ceca1f15c41e9bb465d546c892ef971f7
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-09 08:00:19 +0300

    include django-filter

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',  # Token Authentication with rest_framework.authentication.TokenAuthentication
     'rest_framework_simplejwt',
     'user',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
